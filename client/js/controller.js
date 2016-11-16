848deff74cd093d0aaa7a15aaea35b3c6c78379b
Deepankar01 <deepankar01agrawal@gmail.com>
2016-11-17 02:24:30 +0530

    fixed the when we go back bug

--- client/js/controller.js
+++ client/js/controller.js
@@ -2,7 +2,7 @@
  * Created by Agrawal Deepankar on 16/11/2016.
  */
 
-jeenee.controller('appController',function ($scope,$http) {
+jeenee.controller('appController', function ($scope, $http) {
 
     var self = this;
     self.author = '';
@@ -13,18 +13,18 @@ jeenee.controller('appController',function ($scope,$http) {
 
         // console.log(self.query.length);
 
-        if(!self.query){
+        if (!self.query || self.query.length < 5) {
             console.log("reached");
             self.author = '';
             self.imageURL = '';
             self.thought = '';
             self.isMotivateCard = false;
         }
-        if(self.query && self.query.length>5){
+        if (self.query && self.query.length > 5) {
 
-            $http.get('http://localhost:9000/api/ask?q='+ encodeURIComponent(self.query)).then(function (data) {
-                if(data.data.searchResult){
-                    if (data.data.intentType == 'motivate'){
+            $http.get('http://localhost:9000/api/ask?q=' + encodeURIComponent(self.query)).then(function (data) {
+                if (data.data.searchResult && self.query.length > 5) {
+                    if (data.data.intentType == 'motivate') {
                         self.isMotivateCard = true;
                         self.author = data.data.searchResult.author;
                         self.imageURL = data.data.searchResult.imageURL;
